ef7e3b3de9e0c00209263bca3196d7a03f517aec
Mimi <mimivto2007@gmail.com>
2025-01-22 14:59:01 -0800

    more stuff

--- Grade/main.swift
+++ Grade/main.swift
@@ -6,8 +6,16 @@
 //
 
 import Foundation
+import CSV
 
 //VARIABLES
+do{
+    let stream = InputStream(fileAtPath: "/Users/StudentPM/Desktop/students.csv")
+    let csv = try CSVReader(stream: stream!)
+}
+catch{
+    print("Error!")
+}
 
 
 var firstTime = true
@@ -25,11 +33,80 @@ func menu(){
     
     if let userInput = readLine(), let number = Int(userInput), number<10{
         if number == 1{
-            singleStudentGrade()
+            gradeOfStudent()
+        }
+        if number == 2{
+            allGradesForStudent()
+        }
+        if number == 3{
+            allStudentGrades()
+        }
+        if number == 4{
+            averGradesForClass()
+        }
+        if number == 5{
+            averGradeForAssignment()
+        }
+        if number == 6{
+            lowestGradeForClass()
+        }
+        if number == 7{
+            highestGradeForClass()
+        }
+        if number == 8{
+            filterStudents()
+        }
+        if number == 9{
+            Quit()
         }
     }
 }
 
-func singleStudentGrade(){
+func gradeOfStudent(){
+    print("Which student would you like to choose?")
     
+    if let studentName = readLine(){
+        
+    }
+    
+    menu()
+}
+
+func allGradesForStudent(){
+    
+    menu()
+}
+
+func allStudentGrades(){
+    
+    menu()
+}
+
+func averGradesForClass(){
+    
+    menu()
+}
+
+func averGradeForAssignment(){
+    
+    menu()
+}
+
+func lowestGradeForClass(){
+    
+    menu()
+}
+
+func highestGradeForClass(){
+    
+    menu()
+}
+
+func filterStudents(){
+    
+    menu()
+}
+
+func Quit(){
+    print("Have a great rest of your day!")
 }
