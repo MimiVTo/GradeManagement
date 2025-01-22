a29e4455fd5c303b60db024023356253614e63d4
Mimi <mimivto2007@gmail.com>
2025-01-22 13:35:45 -0800

    Starting

---
+++ Grade/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Grade
+//
+//  Created by StudentPM on 1/22/25.
+//
+
+import Foundation
+
+//VARIABLES
+
+
+// FUNCTIONS
+
+func menu(){
+    print("Welcome to the Grade Manager!")
+}
+
